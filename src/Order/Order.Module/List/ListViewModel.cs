41ff063ca5e45fb5beb7b6912df279951879c775
Tim Rudat <timrudat@gmail.com>
2021-08-18 10:12:20 +0200

    Add basic order bounded context structure

---
+++ src/Order/Order.Module/List/ListViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Sample.App.Order.Module.List
+{
+    using Prism.Commands;
+    using Prism.Mvvm;
+
+    public class ListViewModel : BindableBase
+    {
+        public ListViewModel() =>
+            LoadData = new DelegateCommand(
+                () => 
+                );
+
+        public DelegateCommand LoadData { get; }
+    }
+}
---
+++ src/Order/Order.Module/OrderModule.cs
@@ -0,0 +1,12 @@
+﻿namespace Sample.App.Order.Module
+{
+    using Prism.Ioc;
+    using Prism.Modularity;
+
+    public class OrderModule : IModule
+    {
+        public void RegisterTypes(IContainerRegistry containerRegistry) { }
+
+        public void OnInitialized(IContainerProvider containerProvider) { }
+    }
+}
