26261f3da764f7edf6857f67758ca1890e77b29a
Tim Rudat <timrudat@gmail.com>
2021-08-18 02:59:12 +0200

    Install ef core and add CustomerDbContext

---
+++ src/Customer/Customer.Data/CustomerDbContext.cs
@@ -0,0 +1,34 @@
+﻿namespace Sample.App.Customer.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using Models;
+
+    public sealed class CustomerDbContext : DbContext
+    {
+        public CustomerDbContext() { }
+
+        public CustomerDbContext(DbContextOptions<CustomerDbContext> options)
+            : base(
+                options
+            ) { }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder
+        )
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite(
+                    "Data Source=Customers.db;Version=3;Journal Mode=Persist;"
+                );
+            }
+
+            base.OnConfiguring(
+                optionsBuilder
+            );
+        }
+    }
+}
---
+++ src/Customer/Customer.Server/CustomerService.cs
@@ -0,0 +1,50 @@
+﻿namespace Sample.App.Customer.Server
+{
+    using System.Threading.Tasks;
+
+    using Data;
+    using Data.Service;
+
+    using Grpc.Core;
+
+    using Customer = Data.Models.Customer;
+
+    public class CustomerService
+        : Data.Service.CustomerService.CustomerServiceBase
+    {
+        private readonly CustomerDbContext _dbContext;
+
+        public CustomerService(CustomerDbContext dbContext) =>
+            _dbContext = dbContext;
+
+        public override async Task<CreateResponse> Create(
+            CreateRequest request,
+            ServerCallContext context
+        )
+        {
+            var grpcCustomer = request.Customer;
+
+            var customer = new Customer
+            {
+                Active = grpcCustomer.Active,
+                CompanyName = grpcCustomer.CompanyName,
+                CreatedAt = grpcCustomer.CreatedAt.ToDateTime(),
+                FirstName = grpcCustomer.FirstName,
+                LastName = grpcCustomer.LastName,
+                UpdatedAt = grpcCustomer.UpdatedAt.ToDateTime()
+            };
+
+            await _dbContext.Customers.AddAsync(
+                customer
+            );
+
+            await _dbContext.SaveChangesAsync();
+
+            var response = new CreateResponse();
+
+            return await Task.FromResult(
+                response
+            );
+        }
+    }
+}
